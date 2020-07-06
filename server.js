7bdbad4b6c6268e1c73b0c32c20719bc84c2c176
Valmik Vyas <valmik.vyas1@gmail.com>
2020-07-06 17:30:24 -0500

    stackathon

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require("express");
+const app = express();
+const path = require("path");
+const db = require("./db");
+app.use(express.json());
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(8098);
+app.use((req, res, next) => {
+  next({
+    status: 404,
+    message: `Page not found for ${req.method} ${req.url}`
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send({
+    message: err.message || JSON.stringify(err)
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+db.sync().then(() => {
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
+});
